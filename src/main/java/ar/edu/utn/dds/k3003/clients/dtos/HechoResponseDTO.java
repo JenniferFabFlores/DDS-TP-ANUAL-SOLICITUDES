025927b182b4ff24fd0d5640f7a3cc1cfa3e849c
NazarethEliasNalbandian <103146813+NazarethEliasNalbandian@users.noreply.github.com>
2025-09-11 15:45:02 -0300

    1

--- src/main/java/ar/edu/utn/dds/k3003/clients/dtos/HechoResponseDTO.java
+++ src/main/java/ar/edu/utn/dds/k3003/clients/dtos/HechoResponseDTO.java
@@ -5,12 +5,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record HechoResponseDTO(
-    @JsonProperty("id") String id,
-    @JsonProperty("nombre_coleccion") String nombreColeccion,
-    @JsonProperty("titulo") String titulo,
-    @JsonProperty("etiquetas") List<String> etiquetas,
-    @JsonProperty("categoria") String categoria,
-    @JsonProperty("ubicacion") String ubicacion,
-    @JsonProperty("fecha") LocalDateTime fecha,
-    @JsonProperty("origen") String origen
+        @JsonProperty("id") String id,
+        @JsonProperty("nombre_coleccion") String nombreColeccion,
+        @JsonProperty("titulo") String titulo,
+        @JsonProperty("etiquetas") List<String> etiquetas,
+        @JsonProperty("categoria") String categoria,
+        @JsonProperty("ubicacion") String ubicacion,
+        @JsonProperty("fecha") LocalDateTime fecha,
+        @JsonProperty("origen") String origen,
+        @JsonProperty("estado") String estado
 ) {}
