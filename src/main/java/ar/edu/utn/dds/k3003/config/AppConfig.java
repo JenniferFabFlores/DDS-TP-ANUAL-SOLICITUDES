d709fae42332e5f904a60582230b07f3e0223ba2
NazarethEliasNalbandian <103146813+NazarethEliasNalbandian@users.noreply.github.com>
2025-09-11 11:54:50 -0300

    corrijo error en constructor

--- src/main/java/ar/edu/utn/dds/k3003/config/AppConfig.java
+++ src/main/java/ar/edu/utn/dds/k3003/config/AppConfig.java
@@ -2,6 +2,7 @@ package ar.edu.utn.dds.k3003.config;
 
 import ar.edu.utn.dds.k3003.app.Fachada;
 import ar.edu.utn.dds.k3003.clients.FuentesProxy;
+import ar.edu.utn.dds.k3003.repository.SolicitudRepository; // 👈 importante: mismo paquete que tu repo
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,9 +16,9 @@ public class AppConfig {
     }
 
     @Bean
-    public Fachada fachada(FuentesProxy fuentesProxy) {
-        Fachada fachada = new Fachada();
+    public Fachada fachada(SolicitudRepository repo, FuentesProxy fuentesProxy) {
+        Fachada fachada = new Fachada(repo);
         fachada.setFachadaFuente(fuentesProxy);
         return fachada;
     }
-} 
+}
